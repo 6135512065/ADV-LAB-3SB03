18f1e1f72eb1002d65df254f9c1c836acea0ecd3
6135512065 <lan_6744@hotmail.com>
2021-02-11 16:49:54 +0700

    งานท้ายการทดลอง

---
+++ resources/views/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML>  
+<html>
+    <head>
+      <h1 style="text-align:center;">Send an Email in Laravel</h1>
+      <h2 style="text-align:center;">Make By : Faruslan Waming</h2>
+      <h2 style="text-align:center;">Student ID : 6135512065</h2>
+    </head>
+      
+  <body style="background-color:powderblue;text-align:center; padding: 40px;">
+    <form name="form1" method="get" action="/sendbasicemail">
+      <h2 style="text-align: center;color:red">Sendbasicemail</h2>
+        <label for="name">Enter Name :</label><br>
+        <input type="text" name="name" size="30" ><br><br>
+        <label for="email">Enter E-mail :</label><br>
+        <input type="text" name="email" size="30" ><br><br>
+        <label for="message">Enter Message :</label><br>
+        <input type="text" name="message" size="30" ><br><br>
+        <input type="submit" value="Send">
+    </form>
+  </body>
+</html>
